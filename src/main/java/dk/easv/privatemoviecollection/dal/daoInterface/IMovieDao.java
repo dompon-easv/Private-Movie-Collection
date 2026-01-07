cdadfbd6c2565b426a76f275121cc11246e358d6
federicobac <federico.baccaro1@gmail.com>
2026-01-07 14:45:05 +0100

    tblMovie + OpenInApp
    display movie in the tblMovie and make movie to play in Desktop App after selecting it from the table

--- src/main/java/dk/easv/privatemoviecollection/dal/daoInterface/IMovieDao.java
+++ src/main/java/dk/easv/privatemoviecollection/dal/daoInterface/IMovieDao.java
@@ -1,9 +1,11 @@
 package dk.easv.privatemoviecollection.dal.daoInterface;
 
 import dk.easv.privatemoviecollection.model.Movie;
+import javafx.collections.ObservableList;
 
 import java.sql.SQLException;
 
 public interface IMovieDao {
     void addMovie(Movie movie) throws SQLException;
+    ObservableList<Movie> getAllMovies() throws SQLException;
 }
