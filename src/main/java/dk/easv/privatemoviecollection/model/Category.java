8f8d9289fe6cd75d25edc5cd305d5d5fafcf30fe
taszaszasza <nwburda@outlook.com>
2026-01-07 13:23:13 +0100

    category list in new movie screen

--- src/main/java/dk/easv/privatemoviecollection/model/Category.java
+++ src/main/java/dk/easv/privatemoviecollection/model/Category.java
@@ -12,4 +12,9 @@ public class Category {
     public Category (String name, int id)
     { this.name = name;
         this.id = id; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
