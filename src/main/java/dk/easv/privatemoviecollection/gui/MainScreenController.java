dd8ad17189f7951f5487c31b10c2ec7e6f5083e2
G25Harold <71819862+G25Harold@users.noreply.github.com>
2026-01-14 15:17:16 +0100

    Update MainScreenController.java

--- src/main/java/dk/easv/privatemoviecollection/gui/MainScreenController.java
+++ src/main/java/dk/easv/privatemoviecollection/gui/MainScreenController.java
@@ -13,6 +13,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -176,12 +177,16 @@ public class MainScreenController implements Initializable {
 
 
 
-    public void loadMovies() throws SQLException {
-        tblMovies.setItems(
-                FXCollections.observableArrayList(
-                        movieManager.getAllMovies()
-                )
-        );
+    public void loadMovies()  {
+        try{
+            tblMovies.setItems(
+                    FXCollections.observableArrayList(
+                            movieManager.getAllMovies()
+                    )
+            );
+        }catch (MovieException e){
+            AlertHelper.showAlert(e.getMessage());
+        }
     }
 
     public Category getSelectedCategory() {
