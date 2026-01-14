726998dbbcfe5b90b7fe7e47bcad0816c693f0ee
G25Harold <71819862+G25Harold@users.noreply.github.com>
2026-01-14 15:27:41 +0100

    Update MainScreenController.java

--- src/main/java/dk/easv/privatemoviecollection/gui/MainScreenController.java
+++ src/main/java/dk/easv/privatemoviecollection/gui/MainScreenController.java
@@ -13,6 +13,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
